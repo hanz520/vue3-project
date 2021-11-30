e51802236d52592492e5b2c9a16ee412df499229
hanz520 <569691266@qq.com>
2021-11-30 10:54:44 +0800

    chore: 添加commitlint和husky规范git提交信息格式

---
+++ commitlint.config.js
@@ -0,0 +1,41 @@
+/**
+ * feat：新功能
+ * perf：优化某功能
+ * fix：修补某功能的bug
+ * refactor：重构某个功能
+ * test：测试相关的更改
+ * style：仅样式改动
+ * docs：仅文档新增/改动
+ * chore：构建过程或辅助工具的变动
+ * ci：更改配置或脚本文件
+ * revert：回滚
+ * build：影响构建的修改
+ */
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    'type-enum': [2, 'always', 
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
+  }
+}
