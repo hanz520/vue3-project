a31c64a80bc421fd5e33d8b1c32411ce2c1838ad
hanz520 <569691266@qq.com>
2021-12-15 19:35:00 +0800

    feat: logo组件

---
+++ src/layout/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-logo"><img src="@/assets/logo.svg" />数据管理中心</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss">
+.app-logo {
+  width: 100%;
+  height: 50px;
+  text-align: center;
+  line-height: 50px;
+  color: $text-color;
+  font-size: 16px;
+  img {
+    vertical-align: middle;
+    height: 32px;
+    margin-right: 10px;
+  }
+}
+</style>
