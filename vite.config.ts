fdc5271695e084a5926750959c40fbc34ec62882
hanz520 <569691266@qq.com>
2022-01-12 16:55:08 +0800

    ci: 解决unplugin-vue-components按需引入，Notification等组件无法引入样式的问题

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,27 @@ import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import styleImport, { AndDesignVueResolve } from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Components({ resolvers: [AntDesignVueResolver()] })],
+  plugins: [
+    vue(),
+    Components({ resolvers: [AntDesignVueResolver()] }),
+    styleImport({
+      resolves: [AndDesignVueResolve()],
+      libs: [
+        // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
+        {
+          libraryName: 'ant-design-vue',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `ant-design-vue/es/${name}/style/index.css`
+          }
+        }
+      ]
+    })
+  ],
   base: './',
   resolve: {
     alias: {
