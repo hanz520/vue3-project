ee513c2df9fccf1a9e34da18972711c81c6470b6
hanz520 <569691266@qq.com>
2022-01-13 10:50:46 +0800

    feat: 配置所有的antd组件为中文

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <router-view></router-view>
+  <a-config-provider :locale="zhCN" :auto-insert-space-in-button="false">
+    <router-view></router-view>
+  </a-config-provider>
 </template>
 
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import useStorage from './composition/hooks/useStorage'
 import { useUserStore } from './store'
 
