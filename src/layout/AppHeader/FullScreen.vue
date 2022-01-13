baeb337d329aa5ff557b94024df7930e0f9c295f
hanz520 <569691266@qq.com>
2022-01-13 15:44:09 +0800

    feat: 浏览器全屏

--- src/layout/AppHeader/FullScreen.vue
+++ src/layout/AppHeader/FullScreen.vue
@@ -1,11 +1,34 @@
 <template>
-  <span class="app-full-screen">
-    <SvgIcon class="app-full-screen__icon" href="icon-compress" />
+  <span class="app-full-screen" @click="doToggle">
+    <SvgIcon class="app-full-screen__icon" :href="icon" />
   </span>
 </template>
 
 <script lang="ts" setup>
+import screenfull from 'screenfull'
 import SvgIcon from '@/components/svgIcon/SvgIcon.vue'
+import { computed, Ref, ref } from 'vue'
+import { Modal } from 'ant-design-vue'
+const isFullscreen: Ref<boolean> = ref(false)
+
+const icon = computed(() => {
+  return isFullscreen.value ? 'icon-compress' : 'icon-expend'
+})
+
+screenfull.onchange(() => {
+  isFullscreen.value = screenfull.isFullscreen
+})
+
+const doToggle = () => {
+  if (!screenfull.isEnabled) {
+    Modal.info({
+      title: '提示',
+      content: '您的浏览器不支持全屏'
+    })
+    return false
+  }
+  screenfull.toggle()
+}
 </script>
 
 <style lang="scss">
