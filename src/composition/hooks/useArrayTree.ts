b36a2e9df805217aac0a00d6643321bb366e598b
hanz520 <569691266@qq.com>
2022-01-19 17:45:53 +0800

    perf: perf: useArrayTree泛型优化

--- src/composition/hooks/useArrayTree.ts
+++ src/composition/hooks/useArrayTree.ts
@@ -2,9 +2,9 @@ interface AT {
   [k: string]: any
 }
 
-// interface extraFn {
-//   <T>(current: T, parent?: T): T
-// }
+interface extraFn<K> {
+  (current: K, parent?: K): K
+}
 interface ArrayTreeFn {
   <T extends AT>(treeArray: T[], extra?: (current: T, parent?: T) => T, parent?: T): T[]
 }
@@ -20,11 +20,7 @@ export default function useArrayTree(): ArrayTreeController {
    * @param extra  用于再进行才做的时候,给每一次遍历附加进去对应的逻辑
    * @returns
    */
-  const treeToArray: ArrayTreeFn = <T extends AT>(
-    treeArray: T[],
-    extra?: (current: T, parent?: T) => T,
-    parent?: T
-  ) => {
+  const treeToArray: ArrayTreeFn = <T extends AT>(treeArray: T[], extra?: extraFn<T>, parent?: T) => {
     return treeArray.reduce((result: T[], current: T) => {
       const { children, ...others } = current
       let currentCopy = { ...others } as T
